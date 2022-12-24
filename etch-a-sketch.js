8b488a3c941a97bb0646f74ab4a95498edf982c8
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-23 23:10:18 -0300

    Delete if statement and add a class to each div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,6 @@
 const container = document.querySelector('#container');
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
+    div.classList.add('grid-item');
     container.appendChild(div);
-}
-if (div.matches(':hover')) {
-div.classlist.toggle('green');
 }
