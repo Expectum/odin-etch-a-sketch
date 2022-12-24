16a326213a3f0981e392d24b9deab1140b4a5ab0
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-24 00:06:28 -0300

    Add a function to highlight an item

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('#container');
 createDivGrid();
 const grindItems = document.querySelectorAll('.grid-item');
+
+
+
 function createDivGrid() {
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     container.appendChild(div);
 }
+}
+
+function highlight(item) {
+    item.classList.add('highlight');
 }
