6dcb12e2b6f4b90ba9e4148c0b6fe3a1eff2d7f3
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-23 23:20:13 -0300

    Add a function for the for loop and add a constant

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,10 @@
 const container = document.querySelector('#container');
+createDivGrid();
+const grindItems = document.querySelectorAll('.grid-item');
+function createDivGrid() {
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-item');
     container.appendChild(div);
+}
 }
