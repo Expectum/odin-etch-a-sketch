12282d08fc0c39bc53c73f6c8904ac5b75663c5d
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-27 13:57:07 -0300

    Randomize and darken background color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,26 @@
 const container = document.querySelector('#container');
 const button = document.querySelector('button');
-let isFirstGrid = true;
+let darkenAmount = 25.5;
 createDivGrid(16);
 let gridItems = document.querySelectorAll('.grid-item');
 highlightItems();
+
+function createDivGrid(amount) {
+for (let i = 1; i <= (amount * amount); i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
+    div.style.height = `${100/amount}%`;
+    div.style.width = `${100/amount}%`;
+}
+}
+
 function random(min, max) {
     const num = Math.floor(Math.random() * (max - min)) + min;
     return num;
   }
-  
-  function randomColor() {
+
+function randomColor() {
     return (
       "rgb(" +
       random(0, 255) +
@@ -20,33 +31,60 @@ function random(min, max) {
       ")"
     );
   }
+
+  function getLowestMiddleHighest(rgbIntArray) {
+    let highest = {val:-1,index:-1};
+    let lowest = {val:Infinity,index:-1};
+    rgbIntArray.map((val,index)=>{
+        if(val>highest.val){
+          highest = {val:val,index:index};
+        }
+        if(val<lowest.val){
+          lowest = {val:val,index:index};
+        }
+      });
+      if(lowest.index===highest.index){
+        lowest.index=highest.index+1;
+      }
+      let middle = {index: (3 - highest.index - lowest.index)};
+      middle.val = rgbIntArray[middle.index];
+      return [lowest,middle,highest];
+    }    
+
+function darkenByAmount(rgb, darkenAmount) {
+    const rgbIntArray = rgb.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e));
+    const [lowest,middle,highest]=getLowestMiddleHighest(rgbIntArray);
+    if(highest.val===0){
+        return rgb;
+      }
+    const returnArray = [];
+    returnArray[highest.index] = highest.val-(Math.min(highest.val,darkenAmount));
+    const decreaseFraction  =(highest.val-returnArray[highest.index])/ (highest.val);
+    returnArray[middle.index]= middle.val -middle.val*decreaseFraction; 
+    returnArray[lowest.index]= lowest.val -lowest.val*decreaseFraction;              
+    return (`rgb(${returnArray.join()}) `);
+    }
+
 function highlightItems(){
-gridItems.forEach(gridItem => {
-    gridItem.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = randomColor();
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener("mouseover", (e) => {
+            rgbString = randomColor();            
+            e.target.style.backgroundColor = darkenByAmount(rgbString,darkenAmount);
+            if (darkenAmount != 255) {
+                darkenAmount += 25.5;
+                }
+        });
     });
-});
-}
-
-function createDivGrid(amount, isFirstGrid) {
-for (let i = 1; i <= (amount * amount); i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    container.appendChild(div);
-    div.style.height = `${100/amount}%`;
-    div.style.width = `${100/amount}%`;
-}
-}
-
-
+  }
+  
 button.addEventListener('click', () => {
     gridItems.forEach(gridItem => {
         gridItem.remove();
     });
     amount = prompt('Enter the number of squares that you want for the new grid:');
-    isFirstGrid = false;
-    createDivGrid(amount, isFirstGrid);
+    if (amount <= 100) {
+        createDivGrid(amount);
+    }  
     gridItems = document.querySelectorAll('.grid-item');
     highlightItems();
-
 })
